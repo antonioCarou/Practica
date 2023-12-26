297b50c0aba82de3c418a5f2fb92013ac4cbd0e7
Antonio Carou <antonio.carou@gmail.com>
2023-12-26 09:32:15 +0100

    Commit Practica

--- src/main/java/com/uoc/practica/controller/NewsController.java
+++ src/main/java/com/uoc/practica/controller/NewsController.java
@@ -2,6 +2,9 @@ package com.uoc.practica.controller;
 
 import com.uoc.practica.model.New;
 import java.util.List;
+
+import com.uoc.practica.model.NewsRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
@@ -11,28 +14,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class NewsController {
-  @Autowired private JdbcTemplate jdbcTemplate;
+  @Autowired NewsRepository newsRepository;
 
   @GetMapping("/news")
   public String showNew(@RequestParam(value = "id") Long id, Model modelView) {
 
-    String sql = "SELECT * FROM news WHERE id = ?";
-    List<New> newsList =
-        jdbcTemplate.query(
-            sql,
-            new Long[] {id},
-            (rs, rowNum) ->
-                New.builder()
-                    .id(rs.getLong("id"))
-                    .title(rs.getString("title"))
-                    .body(rs.getString("body"))
-                    .dateTime(rs.getTimestamp("date_time").toLocalDateTime())
-                    .build());
-    if (!newsList.isEmpty()) {
-      modelView.addAttribute("news", newsList.get(0));
-      return "news";
-    } else {
-      return "news-not-found";
-    }
+    New aNew = newsRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+    modelView.addAttribute("news", aNew);
+    return "news";
   }
 }
---
+++ src/main/java/com/uoc/practica/model/NewsRepository.java
@@ -0,0 +1,5 @@
+package com.uoc.practica.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NewsRepository extends JpaRepository<New, Long> {}
